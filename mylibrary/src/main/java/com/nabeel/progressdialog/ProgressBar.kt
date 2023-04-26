13e9fd5915a54b86360014ddf91bb2175a668336
nabeel <nabeelunitykhan@gmail.com>
2023-04-26 12:03:49 +0530

    initial commit

---
+++ mylibrary/src/main/java/com/nabeel/progressdialog/ProgressBar.kt
@@ -0,0 +1,20 @@
+package com.nabeel.progressdialog
+
+import android.app.ProgressDialog
+import android.content.Context
+
+class ProgressBar {
+
+    lateinit var progressDialog: ProgressDialog
+
+    fun showDialog(context: Context){
+        progressDialog = ProgressDialog(context)
+        progressDialog.show()
+        progressDialog.setContentView(R.layout.show_dialog)
+        progressDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+
+    fun hideDialog(){
+        progressDialog.dismiss()
+    }
+}
